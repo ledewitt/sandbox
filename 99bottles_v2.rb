58d8f5cb0abe95bb29f1468857380572a3ffea55
Luke DeWitt <Luke@luke-dewitts-macbook-pro.local>
2012-01-12 14:39:39 -0700

    adding a new and improved 99bottles to this repo

---
+++ 99bottles_v2.rb
@@ -0,0 +1,6 @@
+99.downto(1) do |bottles|
+  puts "#{bottles} bottle#{'s' if bottles !=1} of beer on the wall"
+  puts 'Take one down pass it around'
+end
+
+puts 'No more bottles on the wall we are all drunk now go home!'
