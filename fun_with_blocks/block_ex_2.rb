dcbd54153639b432e49e9623750baf3d1b90811b
Luke DeWitt <Luke@luke-dewitts-macbook-pro.local>
2012-01-13 10:59:54 -0700

    working with block examples from Eloquent Ruby book

---
+++ fun_with_blocks/block_ex_2.rb
@@ -0,0 +1,8 @@
+def print_the_value_returned_by_the_block
+  if block_given?
+    value = yield
+    puts "The block returned #{value}"
+  end
+end
+
+print_the_value_returned_by_the_block {3.14195 / 4.0 }
