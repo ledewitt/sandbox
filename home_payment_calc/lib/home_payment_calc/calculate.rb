73bfca33037fd74ab3f4dc1cf663ccf71db21924
Luke DeWitt <ledewitt@gmail.com>
2012-04-10 09:11:45 -0600

    Some work.

--- home_payment_calc/lib/home_payment_calc/calculate.rb
+++ home_payment_calc/lib/home_payment_calc/calculate.rb
@@ -2,31 +2,50 @@ module HomePaymentCalculator
   class Calculate
   
   def initialize(principle, annual_rate, years)
-    @principle = principle
-    @months = years * 12
-    @monthly_int_rate = annual_rate / 12
-    @half_monthly_int_rate = @monthly_int_rate / 2
-    @bi_weekly_rate = annual_rate / 26
-    @remaining_principle = principle
+    @principle              = principle
+    @annual_rate            = annual_rate
+    @years                  = years
+    @months                 = months(@years)
+    @monthly_int_rate       = monthly_int_rate(@annual_rate)
+    @twice_monthly_int_rate = twice_monthly_int_rate(@monthly_int_rate)
+    @bi_weekly_rate         = bi_weekly_rate(@annual_rate)
+    @remaining_principle    = @principle
     
     puts "Principle #{@principle}"
     puts "Months #{@months}"
     puts "Monthly Interest Rate #{@monthly_int_rate}"
-    puts "Twice a month interest rate #{@half_monthly_int_rate}"
+    puts "Twice a month interest rate #{@twice_monthly_int_rate}"
     puts "Biweekly interest rate #{@bi_weekly_rate}"
   end
   
-  def monthly_payment_information
+  def months (years)
+    years * 12
+  end
+
+  def monthly_int_rate(annual_rate)
+    annual_rate / 12
+  end
   
-    colculated_payment = @principal * ( @monthly_int_rate / ( 1 - ((1 + @monthly_int_rate )**-@months)))
-    calculated_monthly_interest = @monthly_int_rate * @remaining_principle
-    calculated_monthly_principle = 42
-    
+  def twice_monthly_int_rate(monthly_int_rate)
+    monthly_int_rate / 2
   end
-    
-  def loans_remaining_principle
-    
+  
+  def bi_weekly_rate(annual_rate)
+    annual_rate / 26    
   end
-    
+
+  
+  # def monthly_payment_information
+  # 
+  #   colculated_payment = @principal * ( @monthly_int_rate / ( 1 - ((1 + @monthly_int_rate )**-@months)))
+  #   calculated_monthly_interest = @monthly_int_rate * @remaining_principle
+  #   calculated_monthly_principle = 42
+  #   
+  # end
+  #   
+  # def loans_remaining_principle
+  #   
+  # end
+  #   
   end
 end
---
+++ home_payment_calc/spec/calculate_spec.rb
@@ -0,0 +1,18 @@
+require "home_payment_calc/calculator"
+
+describe Mcal::Calculator do
+  let(:calc) { Mcal::Calculator.new(250_000, 0.05, 30) }
+  let(:delta) { 0.00001 }
+  
+  it "calculates monthly interest" do
+    calc.monthly_interest.should be_within(delta).of(0.00416667)
+  end
+  
+  it "calculates twice monthly interest" do
+    twice_monthly_int_rate.should be_within(delta).of(0.00208333)
+  end
+
+  it "calculates biweekly interest" do
+    calc.biweekly_interest.should be_within(delta).of(0.00173077)
+  end
+end
