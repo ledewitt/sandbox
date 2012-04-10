0e1a3394ac96f7cee22592f155b1795dc3610137
Luke DeWitt <ledewitt@gmail.com>
2012-04-10 10:10:43 -0600

    Some work.

--- home_payment_calc/spec/calculate_spec.rb
+++ home_payment_calc/spec/calculate_spec.rb
@@ -1,18 +1,19 @@
 require "home_payment_calc/calculator"
+require "home_payment_calc/calculate"
 
 describe HomePaymentCalculator::Calculator do
   let(:calc) { HomePaymentCalculator::Calculate.new(250_000, 0.05, 30) }
   let(:delta) { 0.00001 }
   
   it "calculates monthly interest" do
-    calc.monthly_interest.should be_within(delta).of(0.00416667)
+    calc.monthly_int_rate.should be_within(delta).of(0.00416667)
   end
   
   it "calculates twice monthly interest" do
-    twice_monthly_int_rate.should be_within(delta).of(0.00208333)
+    calc.twice_monthly_int_rate.should be_within(delta).of(0.00208333)
   end
 
   it "calculates biweekly interest" do
-    calc.biweekly_interest.should be_within(delta).of(0.00173077)
+    calc.bi_weekly_rate.should be_within(delta).of(0.00173077)
   end
 end
