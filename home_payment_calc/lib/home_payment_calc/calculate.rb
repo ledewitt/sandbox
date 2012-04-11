60fd0b7469b629ceb2935f415a2b115799275d39
Luke DeWitt <ledewitt@gmail.com>
2012-04-11 08:25:06 -0600

    Baby Steps.

--- home_payment_calc/lib/home_payment_calc/calculate.rb
+++ home_payment_calc/lib/home_payment_calc/calculate.rb
@@ -6,20 +6,29 @@ module HomePaymentCalculator
     @annual_rate            = annual_rate
     @years                  = years
     @months                 = months
+    @num_biweekly_payments  = num_biweekly_payments
     @monthly_int_rate       = monthly_int_rate
     @twice_monthly_int_rate = twice_monthly_int_rate
-    @bi_weekly_rate         = bi_weekly_rate
+    @biweekly_int_rate      = biweekly_int_rate
     @remaining_principle    = @principle
+    @monthly_payment        = monthly_payment
+    @biweekly_payment       = biweekly_payment
     
     puts "Principle #{@principle}"
     puts "Months #{@months}"
     puts "Monthly Interest Rate #{@monthly_int_rate}"
     puts "Twice a month interest rate #{@twice_monthly_int_rate}"
-    puts "Biweekly interest rate #{@bi_weekly_rate}"
+    puts "Biweekly interest rate #{@biweekly_int_rate}"
+    puts "Monthly payment is #{@monthly_payment}"
+    puts "Monthly biweekly payment is #{@biweekly_payment}"    
   end
   
   def months
     @years * 12
+  end
+  
+  def num_biweekly_payments
+    @years * 26
   end
 
   def monthly_int_rate
@@ -30,9 +39,21 @@ module HomePaymentCalculator
     @monthly_int_rate / 2
   end
   
-  def bi_weekly_rate
+  def biweekly_int_rate
     @annual_rate / 26    
   end
+
+  def monthly_payment
+    @principle * ( @monthly_int_rate / ( 1 - ((1 + @monthly_int_rate )**-@months)))
+  end
+      
+  def biweekly_payment
+    @principle * ( @biweekly_int_rate / ( 1 - ((1 + @biweekly_int_rate )**-@num_biweekly_payments)))
+  end
+
+  #   def twice_monthly_payment
+  #     
+  #   end
 
   
   # def monthly_payment_information
--- home_payment_calc/spec/calculate_spec.rb
+++ home_payment_calc/spec/calculate_spec.rb
@@ -4,6 +4,7 @@ require "home_payment_calc/calculate"
 describe HomePaymentCalculator::Calculator do
   let(:calc) { HomePaymentCalculator::Calculate.new(250_000, 0.05, 30) }
   let(:delta) { 0.00001 }
+  let(:money_delta) { 0.01 }
   
   it "calculates monthly interest" do
     calc.monthly_int_rate.should be_within(delta).of(0.00416667)
@@ -14,6 +15,14 @@ describe HomePaymentCalculator::Calculator do
   end
 
   it "calculates biweekly interest" do
-    calc.bi_weekly_rate.should be_within(delta).of(0.0019230769)
+    calc.biweekly_int_rate.should be_within(delta).of(0.0019230769)
   end
+  
+  it "calculates monthly payment" do
+    calc.monthly_payment.should be_within(money_delta).of(1342.05)
+  end
+  
+  it "calculates biweekly payment" do
+    calc.biweekly_payment.should be_within(money_delta).of(619.11)
+  end  
 end
