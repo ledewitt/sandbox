32d10cca98e81e4e40602108f291b31d90f81216
Luke DeWitt <ledewitt@gmail.com>
2012-09-04 11:22:54 -0600

    Fleshing out the Article Object.

--- Articles_Objects/article.rb
+++ Articles_Objects/article.rb
@@ -2,3 +2,14 @@
 # is read and creates an individual arricle object to be returned by the
 # calling function with in article list and populates the article list
 # object.
+
+class Article
+
+  def initialize ( title, body )
+    @title = title
+    @body  = body
+  end
+  
+  attr_reader :title, :body
+  
+end
--- Articles_Objects/article_list.rb
+++ Articles_Objects/article_list.rb
@@ -1,20 +1,23 @@
+require_relative "article.rb"
+
 class ArticleList
 
   def initialize
-    @articles = Array.new
+    @article_list = Array.new
 
     Dir.foreach ("./articles/") do |file|
       if file.include?(".txt")
-        article = Array.new
-        article << "#{file}"
-        article << File.read("./articles/#{file}")
-        @articles << article
+        article = Article.new( "#{file}",
+                               File.read("./articles/#{file}") )
+      
+        @article_list << article
       end
+ 
     end      
   end
 
   def each(&block)
-    @articles.each(&block)
+    @article_list.each(&block)
   end
 
 end
