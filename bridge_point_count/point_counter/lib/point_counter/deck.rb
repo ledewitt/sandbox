62c44b7fdf8a39d79488ab63394b6c0898d51352
Luke DeWitt <luke@dewittsoft.com>
2013-01-15 12:32:55 -0700

    Adding the deck object

--- bridge_point_count/point_counter/lib/point_counter/deck.rb
+++ bridge_point_count/point_counter/lib/point_counter/deck.rb
@@ -2,7 +2,17 @@ module PointCount
   class Deck
     
     def initialize
-      
+      @deck = [ ]
+      open(File.join(File.dirname(__FILE__), *%w[.. .. data suits.dat])) do |f|
+        f.each do |suit|
+          open(File.join(File.dirname(__FILE__), *%w[.. .. data ranks.dat])) do |g|
+            g.each do |rank|
+              @deck << [rank.strip, suit.strip]
+            end
+          end
+        end
+      end
+      p @deck
     end
   
     def talk
