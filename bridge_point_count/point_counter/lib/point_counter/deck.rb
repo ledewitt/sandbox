d560033e8362efb578ab0a342000f40ede69790c
Luke DeWitt <luke@dewittsoft.com>
2013-01-15 11:21:37 -0700

    Adding the skeleton of deck object to the Bridge High Card Count mini app.

---
+++ bridge_point_count/point_counter/lib/point_counter/deck.rb
@@ -0,0 +1,13 @@
+module PointCount
+  class Deck
+    
+    def initialize
+      
+    end
+  
+    def talk
+      "Sorry I can't do that Dave!"
+    end
+  
+  end
+end
--- bridge_point_count/point_counter/point_counter.rb
+++ bridge_point_count/point_counter/point_counter.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
+require_relative 'lib/point_counter/deck.rb'
+
+deck = PointCount::Deck.new
 
 get("/") {
-  "I am here"
+  "#{deck.talk}"
 }
