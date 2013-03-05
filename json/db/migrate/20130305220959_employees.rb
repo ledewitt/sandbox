8479781111ef21d2287b5b3c8dd0f78c3f9e8052
Luke DeWitt <luke@dewittsoft.com>
2013-03-05 15:11:21 -0700

    Setting up the migration file and model for employees.

---
+++ json/db/migrate/20130305220959_employees.rb
@@ -0,0 +1,7 @@
+class Employees < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
---
+++ json/lib/json/employee.rb
@@ -0,0 +1,14 @@
+module Jason
+  class Employee < ActiveRecord::Base
+    
+    validates_presence_of :first_name,
+                          :last_name,
+                          :department,
+                          :job
+                          
+    attr_accessible :first_name,
+                    :last_name,
+                    :department,
+                    :job
+  end
+end
