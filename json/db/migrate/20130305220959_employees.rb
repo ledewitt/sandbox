2f842810fa519e32bc72db732cf8148e2486d637
Luke DeWitt <luke@dewittsoft.com>
2013-03-05 15:23:28 -0700

    Adding content to the migration file for employees.

--- json/db/migrate/20130305220959_employees.rb
+++ json/db/migrate/20130305220959_employees.rb
@@ -1,7 +1,14 @@
 class Employees < ActiveRecord::Migration
   def up
+    create_table :employees do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :department
+      t.string :job
+    end
   end
 
   def down
+    drop_table :employees
   end
 end
